dda018a9e54ebf6f8564c6f4c24d11c2f5416360
Seth Angell <seth.angell@ncino.com>
2023-11-07 16:13:14 -0500

    refactor(test-action): Object -> Map
    - the object wasn't working, going to try and index with a map now

--- dist/index.js
+++ dist/index.js
@@ -2727,12 +2727,7 @@ exports["default"] = _default;
 
 async function groupCommits(commits) {
   const changelog_sections = new Map()
-  console.log('g_commits', commits)
-  console.log('typeof commits', typeof commits)
-
   const parsed_commits = JSON.parse(commits)
-  console.log('parsed_commits', parsed_commits)
-  console.log('typeof parsed commits', typeof parsed_commits)
 
   for (const commit of parsed_commits) {
     if (Boolean(commit.valid) !== true) continue
@@ -2770,15 +2765,15 @@ function getCommitMapping(type) {
   return commit_mappings[type] ? commit_mappings[type] : 'fix'
 }
 
-const commit_mappings = {
-  feat: '## New Features',
-  fix: '## Bug Fixes',
-  chore: '## Technical Tasks',
-  ci: '## Pipeline Updates',
-  docs: '## Documentation',
-  refactor: '## Refactors',
-  test: '## Testing'
-}
+const commit_mappings = new Map([
+  ['feat', '## New Features'],
+  ['fix', '## Bug Fixes'],
+  ['chore', '## Technical Tasks'],
+  ['ci', '## Pipeline Updates'],
+  ['docs', '## Documentation'],
+  ['refactor', '## Refactors'],
+  ['test', '## Testing']
+])
 
 module.exports = {
   commit_mappings,
