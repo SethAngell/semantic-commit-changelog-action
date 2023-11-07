c4daee0e4ca90b910b23c301420752e32ec05be6
Seth Angell <seth.angell@ncino.com>
2023-11-07 15:51:13 -0500

    feat(test-action): refactor forEachs

--- src/generator.js
+++ src/generator.js
@@ -1,21 +1,20 @@
 async function groupCommits(commits) {
   const changelog_sections = new Map()
 
-  commits
-    .filter(commit => commit.valid)
-    .forEach(commit => {
-      const commit_type = commit.message.split(':')[0].toLocaleLowerCase()
-      if (changelog_sections.get(commit_type)) {
-        const section = changelog_sections.get(commit_type)
-        section.items.push(commit.message)
-      } else {
-        const section = {
-          header: getCommitMapping(commit_type),
-          items: [commit.message]
-        }
-        changelog_sections.set(commit_type, section)
+  for (const commit of commits) {
+    if (Boolean(commit.valid) !== true) continue
+    const commit_type = commit.message.split(':')[0].toLocaleLowerCase()
+    if (changelog_sections.get(commit_type)) {
+      const section = changelog_sections.get(commit_type)
+      section.items.push(commit.message)
+    } else {
+      const section = {
+        header: getCommitMapping(commit_type),
+        items: [commit.message]
       }
-    })
+      changelog_sections.set(commit_type, section)
+    }
+  }
 
   return changelog_sections
 }
@@ -23,13 +22,13 @@ async function groupCommits(commits) {
 async function generateChangelogString(sections) {
   const changelog_lines = []
 
-  Object.keys(commit_mappings).forEach(key => {
+  for (const key of Object.keys(commit_mappings)) {
     if (key in sections) {
       const section = sections.get(key)
       changelog_lines.push(section.header)
       changelog_lines.concat(section.items)
     }
-  })
+  }
 
   return changelog_lines.join('\n')
 }
